82287e6ec33635bac2aa7dbcdc7f913455ef00aa
pietie <pieter@L2.co.za>
2017-02-27 12:36:26 +0200

    added ng animations module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,9 @@
-﻿import { NgModule } from '@angular/core';
+﻿import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { IsoDatePipe } from '../pipes/FromISODate';
 import { LogGrid } from '../controls/LogGrid';
@@ -9,9 +11,9 @@ import { LogGrid } from '../controls/LogGrid';
 import { MsgDialog, PromptDialog  } from '~/dialogs'
 
 @NgModule({
-    imports: [CommonModule, FormsModule, MaterialModule],
+    imports: [CommonModule, FormsModule, MaterialModule, BrowserAnimationsModule],
     declarations: [IsoDatePipe, LogGrid, MsgDialog, PromptDialog],
-    exports: [IsoDatePipe, LogGrid, CommonModule, FormsModule, MaterialModule],
+    exports: [IsoDatePipe, LogGrid, CommonModule, FormsModule, MaterialModule, BrowserAnimationsModule],
     entryComponents: [MsgDialog, PromptDialog]
 
 })
