89419c5d329dd7d40330b7999f90c4d57aeb5848
pietie <pieter@L2.co.za>
2017-05-09 14:55:25 +0200

    exception viewer

--- src/app/exception-viewer/exception-viewer.component.ts
+++ src/app/exception-viewer/exception-viewer.component.ts
@@ -2,7 +2,27 @@ import { Component } from '@angular/core';
 import L2 from "l2-lib/L2";
 
 @Component({
-    templateUrl: './exception-viewer.component.html'
+    templateUrl: './exception-viewer.component.html',
+    styles: [
+`
+    div.exception-detail
+    {
+
+    }
+
+    div.exception-detail .msg
+    {
+        background-color: yellow;
+    }
+
+    div.exception-detail .stack
+    {
+        background-color: orange;
+    }
+
+`
+
+    ]
 })
 export class ExceptionViewerComponent {
 
