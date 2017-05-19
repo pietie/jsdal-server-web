1bab7827164239d235963d3436d70d34ad65a72e
Pieter Lessing <pieter@L2.co.za>
2017-05-19 15:32:43 +0200

    added recent exceptions

--- src/app/exception-viewer/exception-viewer.component.ts
+++ src/app/exception-viewer/exception-viewer.component.ts
@@ -10,14 +10,30 @@ import L2 from "l2-lib/L2";
 
     }
 
+    .head
+    {
+        font-size:1.2em;
+        font-weight:bold;
+    }
+
+    /deep/ span.first-stacktrace-line
+    {
+        font-weight: bold;
+        color: #fff;
+    }
+
     div.exception-detail .msg
     {
-        background-color: yellow;
+        
     }
 
     div.exception-detail .stack
     {
-        background-color: orange;
+        background-color: #1E1E1E;
+        padding: 13px;
+        color: #FF8C8C;
+        overflow-y: scroll;
+        font-family: Consolas, 'Courier New', monospace;
     }
 
 `
@@ -27,11 +43,21 @@ import L2 from "l2-lib/L2";
 export class ExceptionViewerComponent {
 
     public exceptionDetail: any;
+    public recentExceptions: any;
+
+    constructor()
+    {
+
+    }
+
+    ngOnInit()
+    {
+        this.fetchRecentExceptions();
+    }
 
     lookupError(errRef: string) {
         
         L2.fetchJson(`/api/exception/${errRef}`).then((r: any) => {
-            console.log(r.Data.exceptionObject);
             this.exceptionDetail = r.Data;
         });
     }
@@ -39,6 +65,21 @@ export class ExceptionViewerComponent {
     formatStackTrace(st:string)
     {
         if (!st) return null;
-        return st.replace(/\n/gm,"<br/>");
+        let lines = st.split('\n'); //st.replace(/\n/gm,"<br/>");
+
+        if (lines.length >= 1)
+        {
+            lines[1] = '<span class="first-stacktrace-line">' + lines[1] + '</span>';
+        }
+
+        return lines.join("<br/>");
+    }
+
+    fetchRecentExceptions()
+    {
+        L2.fetchJson(`/api/exception/top/200`).then((r:any)=>
+        {
+            this.recentExceptions = r.Data;
+        });
     }
 }
