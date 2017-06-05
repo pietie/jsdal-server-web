d1cd79f74947172a6ce7c086ff518e706609ea05
piete <pieter@L2.co.za>
2017-06-05 14:36:56 +0200

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component  } from '@angular/core';
+﻿import { Component } from '@angular/core';
 
 import L2 from 'l2-lib/L2';
 
@@ -9,10 +9,20 @@ import L2 from 'l2-lib/L2';
 export class HomeComponent {
 
     public statsData: any = null;
+    public isLoadProjectList: boolean = false;
+    public projectList:any[] = null;
 
     ngOnInit() {
         console.log('home.component :: ngOnInit');
         L2.fetchJson('/api/main/stats').then((r: any) => { this.statsData = r.Data; });
+
+        this.isLoadProjectList = true;
+        L2.fetchJson('/api/project').then((r: any) => {
+            this.projectList = r.Data;
+            this.isLoadProjectList = false;
+        }).catch(e => {
+            this.isLoadProjectList = false;
+        });
     }
 
     public getWebServerAge(startedDate) {
