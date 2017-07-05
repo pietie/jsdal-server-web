6fe5f97f569e3a1227fec498d80be293cbd86a8f
piete <pieter@L2.co.za>
2017-07-05 06:59:52 +0200

    implemented worker start/stop

--- src/app/workers/workers.component.ts
+++ src/app/workers/workers.component.ts
@@ -4,19 +4,31 @@ import L2 from 'l2-lib/L2';
 @Component({
     templateUrl: './workers.component.html'
 })
-export class WorkersComponent
-{
+export class WorkersComponent {
     public workerList: any[];
 
     ngOnInit() {
         this.reloadWorkersList();
     }
 
-    private reloadWorkersList()
-    {
-         L2.fetchJson(`/api/workers`).then((r: any) => {
+    private reloadWorkersList() {
+        L2.fetchJson(`/api/workers`).then((r: any) => {
             this.workerList = r.Data;
         });
     }
-    
+
+    startWorker(row) {
+        L2.postJson(`/api/workers/${row.id}/start`).then((r) => {
+            L2.success("Worker started.");
+            this.reloadWorkersList();
+        });
+    }
+
+    stopWorker(row) {
+        L2.postJson(`/api/workers/${row.id}/stop`).then((r) => {
+            L2.success("Worker stopped.");
+            this.reloadWorkersList();
+        });
+    }
+
 }
