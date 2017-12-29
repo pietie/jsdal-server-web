caf44ea791881a48c873c7fc62c2ad74573ddadc
piete <pieter@L2.co.za>
2017-12-29 14:46:43 +0200

    .

--- src/app/controls/log-grid.component.ts
+++ src/app/controls/log-grid.component.ts
@@ -1,4 +1,5 @@
 ﻿import {Component, Output, EventEmitter } from '@angular/core'
+import { DomSanitizer } from "@angular/platform-browser";
 
 enum LogEntryType {
     Info = 10,
@@ -11,7 +12,30 @@ enum LogEntryType {
 @Component({
     selector: 'LogGrid',
     inputs: ['datasource: datasource'],
-    templateUrl: 'log-grid.component.html'
+    templateUrl: 'log-grid.component.html',
+    styles: [`
+      .msg {
+          display: inline-block;
+      }  
+
+      .msg.Exception
+      {
+          background-color: #1E1E1E;
+          padding: 13px;
+          color: #FF8C8C;
+          overflow-y: scroll;
+          font-family: Consolas, 'Courier New', monospace;
+      }
+
+      /deep/ .msg .first-line
+      {
+          color: #fff !important;
+          font-weight: bold !important;
+      }
+
+     
+
+    `]
 })
 
 export class LogGrid {
@@ -22,6 +46,11 @@ export class LogGrid {
     public filteredList: any;
     public LogEntryType = LogEntryType;
     public LogEntryTypeValues = Object.keys(LogEntryType).filter(v => isNaN(parseInt(v)));
+
+    constructor(public domSanitizer: DomSanitizer)
+    {
+
+    }
 
     ngOnInit() {
 
@@ -61,5 +90,45 @@ export class LogGrid {
             this.refresh.emit(null);
         }
     }
+
+    formatMessage(row:any/*msg: string*/) {
+        let msg:string = row.Message;
+
+        if (msg == null) return null;
+        if (row.Type != LogEntryType.Exception) return msg;
+
+
+        let st:string = row.Message;
+        if (!st) return null;
+        let lines = st.split('\n'); 
+
+        if (lines.length > 0)
+        {
+            lines[0] = `<div class="first-line">${lines[0]}</div>`;
+        }
+
+        if (lines.length >= 1) {
+            lines[1] = '<span class="first-stacktrace-line">' + lines[1] + '</span>';
+        }
+
+        return lines.join("<br/>");
+
+        // return this.domSanitizer.bypassSecurityTrustHtml(msg.replace(/##.*##/g, (match) => {
+        //     return `<span class="procName">${match.substr(2, match.length - 4)}</span>`;
+        // }));
+
+    }
+
+    /*formatStackTrace(row:any) {
+        let st:string = row.StackTrace;
+        if (!st) return null;
+        let lines = st.split('\n'); //st.replace(/\n/gm,"<br/>");
+
+        if (lines.length >= 1) {
+            lines[1] = '<span class="first-stacktrace-line">' + lines[1] + '</span>';
+        }
+
+        return lines.join("<br/>");
+    }*/
 
 }
