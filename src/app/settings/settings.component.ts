52ab4939ea05c7be8147b3b739c163baadbad88f
Pieter <pieter@L2.co.za>
2023-05-24 09:00:12 +0200

    Added Effective and Expiry dates

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -3,46 +3,52 @@ import { L2 } from 'l2-lib/L2';
 import { ApiService } from '~/services/api';
 
 @Component({
-    selector: 'Settings',
-    templateUrl: './settings.component.html'
+  selector: 'Settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
 })
 export class Settings {
 
-    availableCerts: any;
+  availableCerts: any;
 
-    bindings: BindingEntry[] = [{ enabled: true, hostname: 'localhost', port: 9086 }, { enabled: false, port: 443 }];
+  bindings: BindingEntry[] = [{ enabled: true, hostname: 'localhost', port: 9086 }, { enabled: false, port: 443 }];
 
-    constructor(public api:ApiService) {
+  constructor(public api: ApiService) {
 
-      this.api.get('/api/settings/bindings').then((r: any) => {
-            //console.log("bindings", r.Data);
-            this.bindings = r.Data;
-        });
+    this.api.get('/api/settings/bindings').then((r: any) => {
+      //console.log("bindings", r.Data);
+      this.bindings = r.Data;
+    });
 
-        this.availableCerts = this.api.get('/api/settings/certs').then((json: any) => {
-            //this.perfraw = json.Data;
-            return json.Data;
-        });
+    this.availableCerts = this.api.get('/api/settings/certs').then((json: any) => {
+      //this.perfraw = json.Data;
+      return json.Data;
+    });
+  }
+
+  saveBindings() {
+    try {
+
+      this.api.post(`/api/settings/bindings`, { body: JSON.stringify(this.bindings) }).then(r => {
+        L2.info("Bindings updated successfully. Please restart service for changes to take effect.");
+      });
+
     }
-
-    saveBindings() {
-        try {
-
-          this.api.post(`/api/settings/bindings`, { body: JSON.stringify(this.bindings) }).then(r => {
-                L2.success("Done!?");
-            });
-
-        }
-        catch (e) {
-            L2.handleException(e);
-        }
+    catch (e) {
+      L2.handleException(e);
     }
+  }
+
+  compareCerts(o1, o2) {
+    return o1?.cert?.Thumbprint?.toLowerCase() == o2?.cert?.Thumbprint?.toLowerCase();
+  }
 
 }
 
 class BindingEntry {
-    public enabled: boolean;
-    public hostname?: string;
-    public port?: number;
-    public certThumb?: string;
+  public enabled: boolean;
+  public hostname?: string;
+  public port?: number;
+  public certThumb?: string;
+  public cert?: any;
 }
